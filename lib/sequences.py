bb120d6e6b32eea20efbe5ac5c9ae135160edd16
Levi Mwendwa <lvmwendwa4@gmail.com>
2024-05-22 17:17:30 +0300

    adds fibbonacchi list function

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib_list = []
+    while len(fib_list) !=length :
+        if len(fib_list) == 0 :
+            fib_list.append(0)
+        elif len(fib_list) == 1:
+            fib_list.append(1)
+        else:
+            new_val=fib_list[-1] + fib_list[-2]
+            fib_list.append(new_val)
+    print(fib_list)
